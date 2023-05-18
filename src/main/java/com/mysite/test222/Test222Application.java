7eca70232df32be39aa271ab4f7f567389566fb7
insung5189 <insung5189@gmail.com>
2023-05-18 12:16:59 +0900

    팀플 전 역량 테스트 시작!

---
+++ src/main/java/com/mysite/test222/Test222Application.java
@@ -0,0 +1,13 @@
+package com.mysite.test222;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test222Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test222Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mysite/test222/Test222ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysite.test222;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test222ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
